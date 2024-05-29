839d647f6bc3a355d50285926eecc03e4907df97
SavageLM <nagol.egavas@gmail.com>
2024-05-29 13:01:46 -0500

    Correcting betty formating

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -13,20 +13,20 @@ void shell_sort(int *array, size_t size)
 	while (inc < size)
 		inc = (3 * inc + 1);
 
-    for (; inc >= 1; inc = inc / 3)
+	for (; inc >= 1; inc = inc / 3)
 	{
-        for (i = inc; i < size; i++)
+		for (i = inc; i < size; i++)
 		{
-            tmp = array[i];
-            for (j = i; j >= inc; j -= inc)
+			tmp = array[i];
+			for (j = i; j >= inc; j -= inc)
 			{
-                if (tmp < array[j - inc])
-                    array[j] = array[j - inc];
-                else 
-                    break;
-            }
-            array[j] = tmp;
-        }
+				if (tmp < array[j - inc])
+					array[j] = array[j - inc];
+				else
+					break;
+			}
+			array[j] = tmp;
+		}
 		if (flag)
 		{
 			for (count = 0; count < size; count++)
